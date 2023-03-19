2d50796228bd0c0eeb4b546c2da64cc9e9a0ce6c
madzaki21 <madzaki2009@gmail.com>
2023-03-19 21:59:32 +0100

    3

--- Program.cs
+++ Program.cs
@@ -6,14 +6,9 @@ using MySqlConnector;
 using Microsoft.ML.Runtime;
 using System.Globalization;
 using Microsoft.ML.Transforms.TimeSeries;
-using Mysqlx.Cursor;
 
 public class Program
 {
-    //páginas de interés:
-    //https://learn.microsoft.com/en-us/dotnet/machine-learning/tutorials/time-series-demand-forecasting
-    //https://learn.microsoft.com/en-us/dotnet/api/microsoft.ml.timeseriescatalog.forecastbyssa?view=ml-dotnet
-
     private static string connectionString = @"server=db4free.net;user=elchinobd;database=elchinobd;password=elchinobd;";
     private static string sqlCommand = "SELECT * FROM airpassengers";
 
@@ -22,7 +17,6 @@ public class Program
         MLContext mlContext = new(seed: 0);
         var data = LoadFromDatabase(connectionString, sqlCommand, mlContext);
         IDataView dataView = FormatIDataView(data, mlContext);
-        var result = dataView.Preview(); // <- poner punto de ruptura para revisar el contenido del dataView
         var splitter = mlContext.Data.TrainTestSplit(dataView, 0.2);
         IDataView trainingDataView = splitter.TrainSet;
         IDataView testingDataView = splitter.TestSet;
@@ -40,15 +34,7 @@ public class Program
                                     );
         SsaForecastingTransformer forecasterTransformer = forecastingPipeline.Fit(trainingDataView);
         Evaluate(testingDataView, forecasterTransformer, mlContext);
-        //var forecastEngine = forecasterTransformer.CreateTimeSeriesEngine<InputModel, OutputModel>(mlContext);  //<- System.MissingMethodException: 
-        //'Method not found: 'Void Microsoft.ML.PredictionEngineBase`2..ctor(Microsoft.ML.Runtime.IHostEnvironment, Microsoft.ML.ITransformer, Boolean, Microsoft.ML.Data.SchemaDefinition, Microsoft.ML.Data.SchemaDefinition)'.'
-
-        /*
-         * NOTA. El fallo de arriba era por una referencia de paquete incorrecta. 
-         * En la configuración del proyecto está comentada la referencia al paquete que estaba provocando la excepción.
-         */
-
-        // Obtenemos la información del constructor TimeSeriesPredictionEngine<InputModel, OutputModel>
+        
         var constructorMethod = typeof(PredictionFunctionExtensions)
                                     .GetMethod(nameof(PredictionFunctionExtensions.CreateTimeSeriesEngine),
                                     new Type[] {
@@ -57,27 +43,16 @@ public class Program
                                         typeof(Boolean),
                                         typeof(SchemaDefinition),
                                         typeof(SchemaDefinition)});
-        // Usamos MakeGenericMethod para declarar los tipos genéricos del método al que estamos llamando
         var constructor = constructorMethod.MakeGenericMethod(new Type[] { typeof(InputModel), typeof(OutputModel) });
-
-        // Invocamos al método que hemos obtenido arriba (Toda esta técnica se llama Reflection)
-#pragma warning disable CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+#pragma warning disable CS8600
         var engine = (TimeSeriesPredictionEngine<InputModel, OutputModel>)constructor.Invoke(forecasterTransformer,
             new object[] { forecasterTransformer, mlContext, true,
-                SchemaDefinition.Create(typeof(InputModel)), SchemaDefinition.Create(typeof(OutputModel)) }); // <- [SOLVED]Parameter count mismatch / Method not found
-#pragma warning restore CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+                SchemaDefinition.Create(typeof(InputModel)), SchemaDefinition.Create(typeof(OutputModel)) });
+#pragma warning restore CS8600
 
         Forecast(testingDataView, 6, engine, mlContext);
     }
 
-
-    /// <summary>
-    /// Los datos que nos llegan de la fuente no están en un formato aceptado por el modelo.
-    /// Es por eso que es necesario formatearlos antes de seguir con el proceso. 
-    /// Leemos los objetos DataModel contenidos en el IDataView idv que se pasa por parámetro,
-    /// convertimos el número de pasajeros a Single y la fecha de string a DateTime.
-    /// </summary>
-    /// <returns> IDataView<InputModel> </returns>
     private static IDataView FormatIDataView(IDataView idv, MLContext mlContext)
     {
         List<InputModel> inputs = new List<InputModel>();
@@ -121,11 +96,10 @@ public class Program
 
     private static void Forecast(IDataView testData, int horizon, TimeSeriesPredictionEngine<InputModel, OutputModel> engine, MLContext mlContext)
     {
-        // Método predict
-#pragma warning disable CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+#pragma warning disable CS8600
         var predictMethod = typeof(TimeSeriesPredictionEngine<InputModel, OutputModel>).GetMethod("Predict", new Type[] { typeof(Int32), typeof(Single) });
         var forecast = (OutputModel)predictMethod.Invoke(engine, new object[] { 6, 0.95f });
-#pragma warning restore CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+#pragma warning restore CS8600
         IEnumerable<string> forecastOutput = mlContext.Data.CreateEnumerable<InputModel>(testData, reuseRowObject: false)
         .Take(horizon)
         .Select((InputModel rental, int index) =>
