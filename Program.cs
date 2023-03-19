a55cc53bcbc8676a87f99d331d1bf15a877a2906
madzaki21 <madzaki2009@gmail.com>
2023-03-19 21:53:06 +0100

    a

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.ML.Data;
 using Microsoft.ML;
 using SeriesTemporales.Model;
-using MySql.Data.MySqlClient;
-using System.Data.SqlClient;
 using System.Data;
-using Microsoft.ML.Transforms.TimeSeries;
-using System.Data.Common;
 using MySqlConnector;
+using Microsoft.ML.Runtime;
+using System.Globalization;
+using Microsoft.ML.Transforms.TimeSeries;
+using Mysqlx.Cursor;
 
 public class Program
 {
@@ -20,15 +20,9 @@ public class Program
     public static void Main(string[] args)
     {
         MLContext mlContext = new(seed: 0);
-        //var dataViewSql = LoadFromDatabase(connectionString, sqlCommand, mlContext);
-        //var result = dataViewSql.Preview(); // <- Extrae correctamente los datos de la base pero la columna Month no la parsea - salta excepción constantemente
-        IDataView dataView = mlContext.Data.LoadFromTextFile<InputModel>
-                                    (
-                                    Path.Combine(Environment.CurrentDirectory, "Data", "AirPassengers.csv"),
-                                    hasHeader: true,
-                                    separatorChar: ','
-                                    );
-        //var result = dataView.Preview();
+        var data = LoadFromDatabase(connectionString, sqlCommand, mlContext);
+        IDataView dataView = FormatIDataView(data, mlContext);
+        var result = dataView.Preview(); // <- poner punto de ruptura para revisar el contenido del dataView
         var splitter = mlContext.Data.TrainTestSplit(dataView, 0.2);
         IDataView trainingDataView = splitter.TrainSet;
         IDataView testingDataView = splitter.TestSet;
@@ -44,16 +38,69 @@ public class Program
                                     horizon: 6,
                                     confidenceLevel: 0.95f
                                     );
-        SsaForecastingTransformer forecasterTransformer = forecastingPipeline.Fit(dataView);
+        SsaForecastingTransformer forecasterTransformer = forecastingPipeline.Fit(trainingDataView);
         Evaluate(testingDataView, forecasterTransformer, mlContext);
-        var forecastEngine = forecasterTransformer.CreateTimeSeriesEngine<InputModel, OutputModel>(mlContext); //<- System.MissingMethodException: 
+        //var forecastEngine = forecasterTransformer.CreateTimeSeriesEngine<InputModel, OutputModel>(mlContext);  //<- System.MissingMethodException: 
         //'Method not found: 'Void Microsoft.ML.PredictionEngineBase`2..ctor(Microsoft.ML.Runtime.IHostEnvironment, Microsoft.ML.ITransformer, Boolean, Microsoft.ML.Data.SchemaDefinition, Microsoft.ML.Data.SchemaDefinition)'.'
-        Forecast(testingDataView, 6, forecastEngine, mlContext);
+
+        /*
+         * NOTA. El fallo de arriba era por una referencia de paquete incorrecta. 
+         * En la configuración del proyecto está comentada la referencia al paquete que estaba provocando la excepción.
+         */
+
+        // Obtenemos la información del constructor TimeSeriesPredictionEngine<InputModel, OutputModel>
+        var constructorMethod = typeof(PredictionFunctionExtensions)
+                                    .GetMethod(nameof(PredictionFunctionExtensions.CreateTimeSeriesEngine),
+                                    new Type[] {
+                                        typeof(ITransformer),
+                                        typeof(IHostEnvironment),
+                                        typeof(Boolean),
+                                        typeof(SchemaDefinition),
+                                        typeof(SchemaDefinition)});
+        // Usamos MakeGenericMethod para declarar los tipos genéricos del método al que estamos llamando
+        var constructor = constructorMethod.MakeGenericMethod(new Type[] { typeof(InputModel), typeof(OutputModel) });
+
+        // Invocamos al método que hemos obtenido arriba (Toda esta técnica se llama Reflection)
+#pragma warning disable CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+        var engine = (TimeSeriesPredictionEngine<InputModel, OutputModel>)constructor.Invoke(forecasterTransformer,
+            new object[] { forecasterTransformer, mlContext, true,
+                SchemaDefinition.Create(typeof(InputModel)), SchemaDefinition.Create(typeof(OutputModel)) }); // <- [SOLVED]Parameter count mismatch / Method not found
+#pragma warning restore CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+
+        Forecast(testingDataView, 6, engine, mlContext);
+    }
+
+
+    /// <summary>
+    /// Los datos que nos llegan de la fuente no están en un formato aceptado por el modelo.
+    /// Es por eso que es necesario formatearlos antes de seguir con el proceso. 
+    /// Leemos los objetos DataModel contenidos en el IDataView idv que se pasa por parámetro,
+    /// convertimos el número de pasajeros a Single y la fecha de string a DateTime.
+    /// </summary>
+    /// <returns> IDataView<InputModel> </returns>
+    private static IDataView FormatIDataView(IDataView idv, MLContext mlContext)
+    {
+        List<InputModel> inputs = new List<InputModel>();
+        int iRow = 0;
+        var FEATURE = Array.ConvertAll(idv.GetColumn<UInt32>("Passengers").ToArray(), item => Convert.ToSingle(item));
+        var TARGET = idv.GetColumn<string>("Month").ToArray();
+        foreach (var row in FEATURE)
+        {
+            var obs = new InputModel();
+            obs.Month = DateTime.ParseExact(TARGET[iRow], "yyyy-MM", CultureInfo.InvariantCulture);
+            obs.Passengers = row;
+            inputs.Add(obs);
+            iRow++;
+        }
+        IEnumerable<InputModel> inputsIEnum = inputs;
+        var definedSchema = SchemaDefinition.Create(typeof(InputModel));
+        IDataView trainingDataView = mlContext.Data.LoadFromEnumerable(inputsIEnum, definedSchema);
+        return trainingDataView;
     }
 
     private static IDataView LoadFromDatabase(string connectionString, string sqlCommand, MLContext mlContext)
     {
-        DatabaseLoader loader = mlContext.Data.CreateDatabaseLoader<InputModel>();
+        DatabaseLoader loader = mlContext.Data.CreateDatabaseLoader<DataModel>();
         DatabaseSource dbSource = new DatabaseSource(MySqlConnectorFactory.Instance, connectionString, sqlCommand);
         IDataView data = loader.Load(dbSource);
         return data;
@@ -74,23 +121,27 @@ public class Program
 
     private static void Forecast(IDataView testData, int horizon, TimeSeriesPredictionEngine<InputModel, OutputModel> engine, MLContext mlContext)
     {
-        OutputModel forecast = engine.Predict();
+        // Método predict
+#pragma warning disable CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+        var predictMethod = typeof(TimeSeriesPredictionEngine<InputModel, OutputModel>).GetMethod("Predict", new Type[] { typeof(Int32), typeof(Single) });
+        var forecast = (OutputModel)predictMethod.Invoke(engine, new object[] { 6, 0.95f });
+#pragma warning restore CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
         IEnumerable<string> forecastOutput = mlContext.Data.CreateEnumerable<InputModel>(testData, reuseRowObject: false)
         .Take(horizon)
         .Select((InputModel rental, int index) =>
         {
-            string rentalDate = rental.Month;
-            float actualRentals = rental.Passengers;
+            string date = rental.Month.ToString(@"yyyy-MM");
+            float actualPassengers = rental.Passengers;
             float lowerEstimate = Math.Max(0, forecast.LowerBoundPassengers[index]);
             float estimate = forecast.ForecastPassengers[index];
             float upperEstimate = forecast.UpperBoundPassengers[index];
-            return $"Date: {rentalDate}\n" +
-            $"Actual Rentals: {actualRentals}\n" +
+            return $"Fecha: {date}\n" +
+            $"Pasajeros reales: {actualPassengers}\n" +
             $"Lower Estimate: {lowerEstimate}\n" +
-            $"Forecast: {estimate}\n" +
+            $"Prediccion: {estimate}\n" +
             $"Upper Estimate: {upperEstimate}\n";
         });
-        Console.WriteLine(" > Rental Forecast");
+        Console.WriteLine(" > Passengers Forecast");
         Console.WriteLine("");
         foreach (var prediction in forecastOutput)
         {
